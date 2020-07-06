12208c9f2ca6deb191781b8f93158d8709ebc8ff
Rodrigo Valentim <rodrigo.valentim@gmail.com>
2020-07-06 18:24:27 -0300

    configuracoes heroku

--- config/logger.js
+++ config/logger.js
@@ -8,13 +8,13 @@ const { createLogger, transports, format } = winston;
 const myFormat = format.printf(({ level, message, label, timestamp }) => {
   return `${timestamp} [${label}] ${level}: ${message}`;
 });
-const URL_DB = 'mongodb+srv://valentim:qpal01qpal01@valentim-ww9xz.mongodb.net/bootcamp?retryWrites=true&w=majority';
+
 const logger = createLogger({
   transports: [
     new transports.Console(),
     new transports.MongoDB({
       level: 'info',
-      db: URL_DB,
+      db: process.env.MONGODB,
       collection: 'logs_grades',
       capped: true,
       cappedMax: 20,
--- models/index.js
+++ models/index.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 import gradesModel from './gradesModel.js';
 
-const URL_DB = 'mongodb+srv://valentim:qpal01qpal01@valentim-ww9xz.mongodb.net/bootcamp?retryWrites=true&w=majority';
-
 const db = {
   mongoose: mongoose,
-  url: URL_DB,
+  url: process.env.MONGODB,
 };
 
 db.grades = gradesModel(mongoose);
